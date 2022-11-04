4dd8f70051bdaf59119ee25e7e87f2d67568f6c6
Esa Firman <esafirm21@gmail.com>
2022-11-04 15:30:50 +0700

    rename the modules

--- example/desktop/src/jvmMain/kotlin/main.kt
+++
@@ -1,9 +0,0 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import nolambda.uibook.sample.App
-
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
-    }
-}
