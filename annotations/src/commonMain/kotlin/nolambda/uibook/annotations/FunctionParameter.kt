381fe4ec1710078c14f33da6159cecd4ecdabfb3
Esa Firman <esafirm21@gmail.com>
2022-12-03 23:03:46 +0700

    better doc

--- annotations/src/commonMain/kotlin/nolambda/uibook/annotations/FunctionParameter.kt
+++ annotations/src/commonMain/kotlin/nolambda/uibook/annotations/FunctionParameter.kt
@@ -1,5 +1,23 @@
 package nolambda.uibook.annotations
 
+/**
+ * Represent each parameter in UI book function
+ *
+ * For example
+ *
+ * ```kotlin
+ * @UIBook
+ * fun BookHost.Text(@Choices(values = arrayOf("a","b")) aText: String = "Some text") {...}
+ * ```
+ *
+ * In above example function parameter will represent `aText` with value
+ *
+ * name: aText
+ * type: String
+ * defaultValue: "Some text"
+ * annotation: Choices(values = arrayOf("a","b"))
+ *
+ */
 data class FunctionParameter(
     val name: String,
     val type: String,
--- browser-core/src/commonMain/kotlin/nolambda/uibook/components/bookform/InputData.kt
+++ browser-core/src/commonMain/kotlin/nolambda/uibook/components/bookform/InputData.kt
@@ -2,6 +2,14 @@ package nolambda.uibook.components.bookform
 
 import nolambda.uibook.browser.form.InputCreator
 
+/**
+ * Represent input data for [BookForm]
+ *
+ * Contains:
+ * - the actual view state that will be passed into book function parameter
+ * - input creators for interacting with input and will call [setViewState] to update the view state
+ * - [setViewState] to update the view state
+ */
 class InputData(
     val viewState: Array<Any>,
     val inputCreator: InputCreator,
