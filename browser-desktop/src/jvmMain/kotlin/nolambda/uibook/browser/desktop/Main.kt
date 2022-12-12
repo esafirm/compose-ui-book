56b89f0cec082c1394681e7dbf7415265ba9bb67
Esa Firman <esafirm21@gmail.com>
2022-12-12 08:48:46 +0700

    make big frame did not overlap book list (menu)

--- browser-desktop/src/jvmMain/kotlin/nolambda/uibook/browser/desktop/Main.kt
+++ browser-desktop/src/jvmMain/kotlin/nolambda/uibook/browser/desktop/Main.kt
@@ -1,8 +1,10 @@
 package nolambda.uibook.browser.desktop
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -12,12 +14,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.simulateHotReload
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.zIndex
 import nolambda.uibook.browser.BookHost
 import nolambda.uibook.browser.EmptyBookHost
 import nolambda.uibook.browser.config.AppBrowserConfig
@@ -67,10 +71,17 @@ fun runBrowser() {
             MaterialTheme {
                 var selectedIndex by remember { mutableStateOf(-1) }
                 Row {
-                    ComponentList(names) { index ->
+                    ComponentList(
+                        names = names,
+                        modifier = Modifier.zIndex(2f)
+                    ) { index ->
                         selectedIndex = index
                     }
-                    ComponentViewer(selectedIndex, library)
+                    ComponentViewer(
+                        modifier = Modifier.zIndex(1f),
+                        selectedIndex = selectedIndex,
+                        library = library
+                    )
                 }
             }
         }
@@ -80,24 +91,32 @@ fun runBrowser() {
 @Composable
 private fun RowScope.ComponentList(
     names: List<String>,
-    onSelected: (index: Int) -> Unit
+    modifier: Modifier = Modifier,
+    onSelected: (index: Int) -> Unit,
 ) {
-    Box(modifier = Modifier.weight(1F)) {
-        BookList(bookNames = names, onSelected)
+    Box(modifier = Modifier.weight(1F).composed { modifier }) {
+        BookList(
+            bookNames = names,
+            modifier = Modifier
+                .fillMaxHeight()
+                .background(MaterialTheme.colors.background),
+            navigateToBook = onSelected
+        )
     }
 }
 
 @Composable
 private fun RowScope.ComponentViewer(
     selectedIndex: Int,
-    library: UIBookLibrary
+    modifier: Modifier = Modifier,
+    library: UIBookLibrary,
 ) {
     val emptyBookConfig = object : BookConfig {
         override val onExit: () -> Unit = {}
     }
 
     Box(
-        modifier = Modifier.weight(3F)
+        modifier = Modifier.weight(3F).composed { modifier }
     ) {
 
         val isSelected = selectedIndex >= 0
