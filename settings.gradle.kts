1962f385234f9b91453cc21347dac7da993fc9e5
Esa Firman <esafirm21@gmail.com>
2022-12-16 15:29:08 +0700

    migrate repositories to settings gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,32 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
         maven {
             setUrl("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        }
+    }
+}
+
+val secretProps = java.util.Properties()
+file("secret.properties").inputStream().use { secretProps.load(it) }
+
+dependencyResolutionManagement {
+    rulesMode.set(RulesMode.FAIL_ON_PROJECT_RULES)
+
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        maven { setUrl("https://jitpack.io") }
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/qawaz/compose-code-editor")
+            credentials {
+                username = secretProps["github.username"] as String
+                password = secretProps["github.token"] as String
+            }
         }
     }
 }
