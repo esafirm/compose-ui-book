d49c26b3971fc1a7f43c43e49d7c0142fb845dc8
Esa Firman <esafirm21@gmail.com>
2022-12-17 21:02:01 +0700

    support building without configuring publishing env

--- build-conventions/src/main/kotlin/nolambda.uibook.publish.gradle.kts
+++ build-conventions/src/main/kotlin/nolambda.uibook.publish.gradle.kts
@@ -11,20 +11,24 @@ plugins {
     signing
 }
 
-val secretPropsFile = project.rootProject.file("local.properties")
-if (secretPropsFile.exists()) {
-    secretPropsFile.reader().use {
-        Properties().apply {
-            load(it)
-        }
-    }.onEach { (name, value) ->
-        extra[name.toString()] = value
+private val propToEnvVarList = listOf(
+    "uibook.enablePublishing" to "ENABLE_PUBLISHING",
+    "signing.password" to "SIGNING_PASSWORD",
+    "signing.key" to "SIGNING_KEY",
+    "ossrhUsername" to "OSSRH_USERNAME",
+    "ossrhPassword" to "OSSRH_PASSWORD"
+)
+
+val secretPropsFile: File = project.rootProject.file("local.properties")
+val localProperties = Properties().apply {
+    if (secretPropsFile.exists()) {
+        secretPropsFile.reader().use { load(it) }
     }
-} else {
-    extra["signing.password"] = System.getenv("SIGNING_PASSWORD")
-    extra["signing.key"] = System.getenv("SIGNING_KEY")
-    extra["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
-    extra["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+}
+
+// Load from local.properties or environment variables
+propToEnvVarList.forEach {
+    extra[it.first] = localProperties[it.first] ?: System.getenv(it.second)
 }
 
 val sourceJar by tasks.registering(Jar::class) {
@@ -35,73 +39,91 @@ val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
 }
 
-val signingKey: String
-    get() {
-        val base64encodedKey = extra["signing.key"]?.toString()
-            ?: error("Signing key is not set. Please check your environment again")
-        return String(Base64.getDecoder().decode(base64encodedKey))
-    }
-
 val uiBookExt = UIBookExt.getOrRegister(project)
 
-publishing {
+fun Project.setupPublishing() {
+    val base64encodedKey = extra["signing.key"]?.toString()
+        ?: error("Signing key is not set. Please check your environment again")
 
-    signing {
-        useInMemoryPgpKeys(
-            signingKey,
-            extra["signing.password"] as String
-        )
-        sign(publishing.publications)
-    }
+    val signingKey = String(Base64.getDecoder().decode(base64encodedKey))
 
-    repositories {
-        maven {
-            name = "sonatype"
-            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = extra["ossrhUsername"]?.toString() ?: error("ossrh username is not set")
-                password = extra["ossrhPassword"]?.toString() ?: error("ossrh password is not set")
+    publishing {
+
+        signing {
+            useInMemoryPgpKeys(
+                signingKey,
+                extra["signing.password"] as String
+            )
+            sign(publishing.publications)
+        }
+
+        repositories {
+            maven {
+                name = "sonatype"
+                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = extra["ossrhUsername"]?.toString() ?: error("ossrh username is not set")
+                    password = extra["ossrhPassword"]?.toString() ?: error("ossrh password is not set")
+                }
             }
         }
-    }
 
-    // Configure all publications
-    publications.withType<MavenPublication> {
+        // Configure all publications
+        publications.withType<MavenPublication> {
 
-        // Configure the publication
-        artifactId = uiBookExt.artifactId.getOrElse(project.name)
-        groupId = rootProject.group.toString()
-        version = rootProject.version.toString()
+            println("Group ID: ${rootProject.group}")
+            println("Root project name: ${rootProject.name}")
 
-        // Stub javadoc.jar artifact needed by sonatype closing process
-        artifact(javadocJar.get())
+            // Configure the publication
+            artifactId = uiBookExt.artifactId.getOrElse(project.name)
+            groupId = rootProject.group.toString()
+            version = rootProject.version.toString()
 
-        // Stub source.jar artifact
-        artifact(sourceJar.get())
+            // Stub javadoc.jar artifact needed by sonatype closing process
+            artifact(javadocJar.get())
 
-        // Provide artifacts information requited by Maven Central
-        pom {
-            name.set("Compose UI Book")
-            description.set("UI Component explorer for Jetpack Compose")
-            url.set("https://github.com/esafirm/compose-ui-book")
+            // Stub source.jar artifact
+            artifact(sourceJar.get())
 
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/licenses/MIT")
-                }
-            }
-            developers {
-                developer {
-                    id.set("esafirm")
-                    name.set("Esa Firman")
-                    email.set("esafirm21@gmail.com")
-                }
-            }
-            scm {
+            // Provide artifacts information requited by Maven Central
+            pom {
+                name.set("Compose UI Book")
+                description.set("UI Component explorer for Jetpack Compose")
                 url.set("https://github.com/esafirm/compose-ui-book")
-            }
 
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("esafirm")
+                        name.set("Esa Firman")
+                        email.set("esafirm21@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/esafirm/compose-ui-book")
+                }
+
+            }
         }
     }
+}
+
+val enablePublishing = extra["uibook.enablePublishing"] as? String == "true"
+if (enablePublishing.not()) {
+    logger.lifecycle(
+        """
+        Publishing is disabled.
+
+        To enable it, set the environment variable ENABLE_PUBLISHING to true.
+        Or set the property `uibook.enablePublishing` to true in local.properties
+
+    """.trimIndent()
+    )
+} else {
+    setupPublishing()
 }
